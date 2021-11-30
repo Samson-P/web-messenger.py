c434c3236fd21ffeb393709aa619283316b5fb64
Pohilenko Samson <samsonpoholenko@gmail.com>
2021-11-30 06:19:06 +0300

    js client prod

--- websocket_client.js
+++ websocket_client.js
@@ -1,8 +1,27 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 	
-	let websocketClient() = new WebSocket("ws://localhost:1090");
+	const messagesContainer = document.querySelector("#messages_container");
+	const messageInput = document.querySelector("[name=message_input]");
+	const sendMessageButton = document.querySelector("[name=send_message_button]");
 	
+	
+	let websocketClient = new WebSocket("ws://localhost:1090");
+	
+	websocketClient.onopen = () => {
+		console.log("Client connected!");
+		
+		sendMessageButton.onclick = () => {
+			websocketClient.send(messageInput.value);
+			messageInput.value = "";
+		};
+	};
+	
+	websocketClient.onmessage = (message) => {
+		const newMessage = document.createElement("div");
+		newMessage.innerHTML = message.data;
+		messagesContainer.appendChild(newMessage);
+	};
 	
 }, false);
 
